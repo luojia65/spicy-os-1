43548718659a3cc099bb00a5340eb6e6e2fe40ca
luojia65 <me@luojia.cc>
2020-06-26 11:54:27 +0800

    Update interrupt module

--- spicy-os/src/main.rs
+++ spicy-os/src/main.rs
@@ -1,11 +1,14 @@
 #![no_std]
 #![no_main]
+#![feature(global_asm, llvm_asm)]
+mod interrupt;
 
 use opensbi_rt::println;
 use opensbi_rt::sbi;
 
 #[opensbi_rt::entry]
 fn main(hartid: usize, dtb: usize) {
+    interrupt::init();
     println!("Hello, OpenSBI!");
     println!("hartid={}, dtb={:#x}", hartid, dtb);
     println!("spec_version = {:?}", sbi::base::get_spec_version());
@@ -14,4 +17,8 @@ fn main(hartid: usize, dtb: usize) {
     println!("mvendorid    = {:?}", sbi::base::get_mvendorid());
     println!("marchid      = {:?}", sbi::base::get_marchid());
     println!("mimpid       = {:?}", sbi::base::get_mimpid());
+    unsafe {
+        llvm_asm!("ebreak"::::"volatile");
+    };
+
 }
