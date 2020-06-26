5dafab99ddfb826827667a9a62651ee9768882e4
luojia65 <me@luojia.cc>
2020-06-26 14:01:27 +0800

    Update interrupt design

--- spicy-os/src/main.rs
+++ spicy-os/src/main.rs
@@ -1,14 +1,14 @@
 #![no_std]
 #![no_main]
 #![feature(global_asm, llvm_asm)]
-mod interrupt;
 
 use opensbi_rt::println;
 use opensbi_rt::sbi;
+use opensbi_rt::interrupt::TrapFrame;
+use riscv::register::scause::Scause;
 
 #[opensbi_rt::entry]
 fn main(hartid: usize, dtb: usize) {
-    interrupt::init();
     println!("Hello, OpenSBI!");
     println!("hartid={}, dtb={:#x}", hartid, dtb);
     println!("spec_version = {:?}", sbi::base::get_spec_version());
@@ -20,5 +20,11 @@ fn main(hartid: usize, dtb: usize) {
     unsafe {
         llvm_asm!("ebreak"::::"volatile");
     };
+}
 
+#[export_name = "ExceptionHandler"]
+pub fn handle_exception(_trap_frame: &TrapFrame, scause: Scause, stval: usize) {
+    panic!(
+        "Exception occurred: {:?}; stval: 0x{:x}", scause.cause(), stval
+    );
 }
